edd382813437cae4b7c43c911cba0932e1a2d0fe
Kerem <keremkyr99@gmail.com>
2025-11-24 20:41:31 +0300

    Update main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -7,7 +7,7 @@ string ucusGuvenligiKontrol(int yuk, int hiz, int yukseklik)
     if (pil < 0)
         pil = 0;
     cout << "Pil Seviyesi: " << pil << "%" << endl;
-    if (yuk > 80)
+    if (yuk > 500)
         return "Agir yuk, ucamaz!";
     else if (pil < 20)
         return "Pil seviyesi dusuk, ucus guvensiz!";
@@ -19,12 +19,16 @@ string ucusGuvenligiKontrol(int yuk, int hiz, int yukseklik)
 
 int main()
 {
-    cout << "\nDrone 1" <<endl;
-    cout << ucusGuvenligiKontrol(60, 40, 200) << endl;
-    cout << "\nDrone 2" <<endl;
-    cout << ucusGuvenligiKontrol(90, 20, 100) << endl;
-    cout << "\nDrone 3" <<endl;
-    cout << ucusGuvenligiKontrol(40, 50, 600) << endl;
+int yukler[3]={675 ,340 ,120};
+int hizler[3]={100, 59, 89};
+int yukseklik[3]={200, 120, 99};
 
+    for(int i = 0;i<3;i++)
+        {
+            cout<<"DRONE: "<< i+1<<endl;
+            cout<<ucusGuvenligiKontrol(yukler[i], hizler[i], yukseklik[i]);
+            cout<<"\n\n";
+        }
     return 0;
+
 }
